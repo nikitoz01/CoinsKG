ef198bca5b240f9a5b44acafa2ec9dfd34f0cbf0
nikita <nikitoz01@gmail.com>
2022-12-19 02:47:03 +0600

    target sdk update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "kg.mobile.coins"
         minSdk = 27
-        targetSdk = 31
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -46,21 +46,21 @@ android {
 dependencies {
 
     // Android
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Fragment and activity
-    implementation("androidx.fragment:fragment-ktx:1.5.1")
-    implementation("androidx.activity:activity-ktx:1.5.1")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.activity:activity-ktx:1.6.1")
 
     // Splashscreen
     implementation("androidx.core:core-splashscreen:1.0.0")
 
     // Fragment navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
 //    implementation ("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
     // Lifecycle and ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
@@ -68,9 +68,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.5.1")
 
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
     // Retrofit and GSON
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -79,21 +83,20 @@ dependencies {
 
     // Glade
     implementation("com.github.bumptech.glide:glide:4.13.0")
-    implementation("androidx.cardview:cardview:1.0.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.13.0")
 
     //Paging3
     implementation("androidx.paging:paging-runtime-ktx:3.1.1")
 
     // Dagger2
-    implementation("com.google.dagger:dagger:2.41")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    kapt("com.google.dagger:dagger-compiler:2.41")
+    implementation("com.google.dagger:dagger:2.44.2")
+    kapt("com.google.dagger:dagger-compiler:2.44.2")
 
     // Room
     implementation("androidx.room:room-runtime:2.4.3")
     annotationProcessor("androidx.room:room-compiler:2.4.3")
     kapt("androidx.room:room-compiler:2.4.3")
+
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:2.4.3")
     // optional - Paging 3 Integration
